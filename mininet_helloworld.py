fb19a3c27dae83a09bb04c366013b8a4af2bacc0
Eric Savage <eric.n.savage2000@gmail.com>
2025-10-02 14:58:38 -0400

    changes made to environment

--- mininet_helloworld.py
+++ mininet_helloworld.py
@@ -1,28 +1,27 @@
 from mininet.net import Mininet
 from mininet.topo import Topo
-from mininet.node import OVSController
+from mininet.node import OVSBridge
 from mininet.cli import CLI
+from mininet.log import setLogLevel
 
 class SingleSwitchTopo(Topo):
     def build(self):
-        # Add two hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
-        # Add a switch
         s1 = self.addSwitch('s1')
-        # Link hosts to switch
         self.addLink(h1, s1)
         self.addLink(h2, s1)
 
 if __name__ == '__main__':
+    setLogLevel('info')
     topo = SingleSwitchTopo()
-    net = Mininet(topo=topo, controller=OVSController)
-    net.start()
-
-    print("*** Running ping test")
-    net.pingAll()
-
-    print("*** Dropping to CLI (type 'exit' to quit)")
-    CLI(net)
-
-    net.stop()
+    # No controller; switch runs as a plain L2 bridge
+    net = Mininet(topo=topo, controller=None, switch=OVSBridge)
+    try:
+        net.start()
+        print("*** Running ping test")
+        net.pingAll()
+        print("*** Dropping to CLI (type 'exit' to quit)")
+        CLI(net)
+    finally:
+        net.stop()
